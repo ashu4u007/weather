bb685b3f92ece45dbe369010bdead990433bd0d2
ashu4u007 <ashutosh10794@gmail.com>
2026-07-28 16:01:01 +0530

    Adding one more comment

--- temp_converter.py
+++ temp_converter.py
@@ -20,7 +20,7 @@ def main():
     print("\n Temprature Converter") # Application for weather temperature 
     print("1. Celcius to fehrenhit") # Celcius to fehrenhit
     print("2. Celsious to kelvin")   # Celsious to kelvin
-    print("3. Fehrenhit to celsius")
+    print("3. Fehrenhit to celsius") # Fehrenhit to celsius
     print("4. Fehrenhite to kelvin")
     print("5. Kelvin to celcius")
     print("6. Kelvin to Fehrenhite")
